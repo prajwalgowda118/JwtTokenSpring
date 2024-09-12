26c90ef778aff915c99267149b1e1c923906e0e7
prajwalgowda118 <prajwal7683@gmail.comgit config --global user.name prajwalgowda118git config --global user.email prajwal7683@gmail.com>
2024-09-12 22:58:40 +0100

    added the initial configuration

---
+++ src/main/java/com/scaler/springsecurity/jwtspring/Model/Role.java
@@ -0,0 +1,9 @@
+package com.scaler.springsecurity.jwtspring.Model;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_MODERATOR,
+    ROLE_ANONYMOUS
+
+}
---
+++ src/main/java/com/scaler/springsecurity/jwtspring/Model/User.java
@@ -0,0 +1,72 @@
+package com.scaler.springsecurity.jwtspring.Model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.naming.Name;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "app_user")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+    @Enumerated(EnumType.STRING)
+
+    private Role role;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()))  ;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+}
